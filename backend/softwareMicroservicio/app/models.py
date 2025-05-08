a7f5c76d1dfd81ff437fc133cb8c56fdfc21a09c
unknown <fabianchors@gmail.com>
2025-05-07 21:19:54 -0500

    se corrigieron cositas

--- backend/softwareMicroservicio/app/models.py
+++ backend/softwareMicroservicio/app/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Software(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     city = db.Column(db.String(30), nullable=False)
